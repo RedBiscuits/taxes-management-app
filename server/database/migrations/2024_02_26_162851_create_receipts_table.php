690190735163b62484e9df61fd88ebe6b0b5631b
RedBiscuits <84057046+RedBiscuits@users.noreply.github.com>
2024-02-26 22:10:39 +0200

    Update: link receipts to days

--- server/database/migrations/2024_02_26_162851_create_receipts_table.php
+++ server/database/migrations/2024_02_26_162851_create_receipts_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('receipts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('location_id')->constrained()->onDelete('cascade');
+            $table->foreignId('day_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
